8f05085386d82fc832a963dcc7760dcd0c60d83c
daniellumertz <daniellumertz2@gmail.com>
2023-07-01 06:58:00 -0300

    Add Render selected items

---
+++ Render/Misc/Functions/Imgui General Functions.lua
@@ -0,0 +1,153 @@
+--@noindex
+--v 0.2
+-- Add many
+
+----------------
+---- Colors
+----------------
+
+---Input a RRGGBB color, from ImGui_ColorEdit3 and output a number color to be used for reaper objects, like markers.
+---@param rgb_input number number from ImGui_ColorEdit3, in the format 0xRRGGBB
+---@return number new_color value to be used in functions like AddProjectMarker2
+function RGBA_To_ReaperRGB(rgb_input)
+    return reaper.ImGui_ColorConvertNative(rgb_input) | 0x1000000
+end
+
+---Input a HSVA color, output is a number to insert in ImGUI widgets (from imgui demo)
+---@param h number hue from 0 - 1
+---@param s number saturation 0 - 1
+---@param v number value 0 - 1
+---@param a number alpha 0 - 1
+---@return number color color for imgui RRGGBBAA
+function HSVtoImGUI(h, s, v, a)
+    local r, g, b = reaper.ImGui_ColorConvertHSVtoRGB(h, s, v)
+    return reaper.ImGui_ColorConvertDouble4ToU32(r, g, b, a or 1.0)
+end
+----------------
+---- Popups
+----------------
+
+function ToolTip(is_tooltip, text, wrap)
+    if is_tooltip and reaper.ImGui_IsItemHovered(ctx) then
+        ToolTipSimple(text, wrap)
+    end
+end
+
+function ToolTipSimple(text, wrap)
+    wrap = wrap or 200    
+    reaper.ImGui_BeginTooltip(ctx)
+    --reaper.ImGui_PushTextWrapPos(ctx, reaper.ImGui_GetFontSize(ctx) * 20)
+    reaper.ImGui_PushTextWrapPos(ctx, wrap)
+    reaper.ImGui_Text(ctx, text)
+    reaper.ImGui_PopTextWrapPos(ctx)
+    reaper.ImGui_EndTooltip(ctx)    
+end
+
+----------------
+---- Keys
+----------------
+
+
+-- PassKeys to the last focused window. Only working in windows currently
+function PassKeys() 
+    --Get keys pressed
+    local active_keys = {}
+    for key_val = 0,255 do
+        if reaper.ImGui_IsKeyPressed(ctx, key_val, true) then -- true so holding will perform many times
+            active_keys[#active_keys+1] = key_val
+        end
+    end
+
+    --Send Message
+    if LastWindowFocus then 
+        if #active_keys > 0  then
+            for k, key_val in pairs(active_keys) do
+                PostKey(LastWindowFocus, key_val)
+            end
+        end
+    end
+
+    -- Get focus window (if not == Script Title)
+    local win_focus = reaper.JS_Window_GetFocus()
+    local win_name = reaper.JS_Window_GetTitle( win_focus )
+
+    -- Need to retain the last window focused that isn't this script! 
+    if LastWindowFocus ~= win_focus and (win_name == 'trackview' or win_name == 'midiview')  then -- focused win title is different? INSERT HERE
+        LastWindowFocus = win_focus
+    end    
+end
+
+-- PassKeys to main or midieditor(if is_midieditor and there is any midi editor active). 
+---@param is_midieditor boolean if true then it will always pass the key presses to the midi editor. If there isnt a midi editor it will pass to the main window. If false pass to the main window
+function PassKeys2(is_midieditor) 
+    --Get keys pressed
+    local active_keys = {}
+    for key_val = 0,255 do
+        if reaper.ImGui_IsKeyPressed(ctx, key_val, true) then -- true so holding will perform many times
+            active_keys[#active_keys+1] = key_val
+        end
+    end
+    -- Get Window
+    local sel_window 
+    if is_midieditor then
+        local midi = reaper.MIDIEditor_GetActive()
+        if midi then 
+            sel_window = midi 
+        end
+    end
+
+    if not sel_window then
+        sel_window = reaper.GetMainHwnd()
+    end
+
+    --Send Message
+    if sel_window then 
+        if #active_keys > 0  then
+            for k, key_val in pairs(active_keys) do
+                PostKey(sel_window, key_val)
+            end
+        end
+    end
+end
+
+function PostKey(hwnd, vk_code)
+    reaper.JS_WindowMessage_Post(hwnd, "WM_KEYDOWN", vk_code, 0,0,0)
+    reaper.JS_WindowMessage_Post(hwnd, "WM_KEYUP", vk_code, 0,0,0)
+end
+
+----------------
+---- Text
+----------------
+
+-- Insert a text at the center of the avail space
+function TextCenter(text)
+    local text_w, text_h = reaper.ImGui_CalcTextSize(ctx, text)
+    local avail_w = reaper.ImGui_GetContentRegionAvail(ctx)
+    reaper.ImGui_SetCursorPosX(ctx, avail_w/2 - text_w/2)
+    reaper.ImGui_Text(ctx, text)
+end
+
+---Print on imgui screen
+function ImPrint(...)
+    local t = {}
+    for i, v in ipairs( { ... } ) do
+        t[i] = tostring( v )
+    end
+    reaper.ImGui_Text(ctx, table.concat( t, " " ))
+end
+
+----------------
+---- Draw
+----------------
+
+
+function DrawRectLastItem(h,s,v,a)
+    local minx, miny = reaper.ImGui_GetItemRectMin(ctx)
+    local maxx, maxy = reaper.ImGui_GetItemRectMax(ctx)
+    local draw_list = reaper.ImGui_GetWindowDrawList(ctx)
+
+    local color =  HSVtoImGUI(h, s, v, a)
+    
+    reaper.ImGui_DrawList_AddRectFilled(draw_list, minx-50, miny, maxx, maxy, color)
+end
+
---
+++ Render/Misc/Render Selected Items In New Tracks.lua
@@ -0,0 +1,64 @@
+--dofile("C:/Users/DSL/AppData/Roaming/REAPER/Scripts/Meus/Debug VS/DL Debug.lua")
+
+function dofile_all(path)
+    local i = 0
+    while true do 
+        local file = reaper.EnumerateFiles( path, i )
+        i = i + 1
+        if not file  then break end 
+        dofile(path..'/'..file)
+    end
+end
+
+local info = debug.getinfo(1,'S')
+local script_path = info.source:match[[^@?(.*[\/])[^\/]-$]] -- this script folder
+
+local folder_name = 'Functions'
+
+dofile_all(script_path..'/'..folder_name)
+
+-------------------------------
+-------   SCRIPT     ----------
+-------------------------------
+if reaper.CountSelectedMediaItems(proj) == 0 then return end
+
+local proj = 0
+
+reaper.Undo_BeginBlock()
+reaper.PreventUIRefresh(1)
+
+--dofile(reaper.GetResourcePath().."/UserPlugins/ultraschall_api.lua")
+local render = reaper.NamedCommandLookup( '_SWS_AWRENDERSTEREOSMART' )
+local sel_items = CreateSelectedItemsTable(proj)
+for k, item in ipairs(sel_items) do
+    local track = reaper.GetMediaItem_Track(item)
+    reaper.SetOnlyTrackSelected(track)
+    -- mute other items in this track
+    local item_table_mute = {}
+    for item_track in enumTrackItems(track) do
+        local mute = reaper.GetMediaItemInfo_Value(item_track, 'B_MUTE')
+        item_table_mute[item_track] = mute
+        if item_track ~= item then
+            reaper.SetMediaItemInfo_Value(item_track, 'B_MUTE', 1)
+        end
+    end
+    -- get info time
+    local pos = reaper.GetMediaItemInfo_Value(item, 'D_POSITION')
+    local len = reaper.GetMediaItemInfo_Value(item, 'D_LENGTH')
+    local fim = pos + len
+    local mute = reaper.GetMediaTrackInfo_Value(track, 'B_MUTE')
+    -- record
+    reaper.GetSet_LoopTimeRange2(proj, true, true, pos, fim, false)
+    reaper.Main_OnCommand(render, 0) --_SWS_AWRENDERSTEREOSMART SWS/AW: Render tracks to stereo stem tracks, obeying time selection
+    reaper.SetMediaTrackInfo_Value(track, 'B_MUTE', mute)
+    reaper.ShowConsoleMsg('halo')
+
+    for restore_item, mute_val in pairs(item_table_mute) do
+        reaper.SetMediaItemInfo_Value(restore_item, 'B_MUTE', mute_val)
+    end
+end
+
+reaper.PreventUIRefresh(-1)
+reaper.UpdateArrange()
+reaper.UpdateTimeline()
+reaper.Undo_EndBlock2(proj, 'Add Time sig to grid', -1)
