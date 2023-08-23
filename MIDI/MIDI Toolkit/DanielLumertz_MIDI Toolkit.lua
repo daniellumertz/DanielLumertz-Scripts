6d41a5407e01fcc8cab1e8e2e6fa92a58aa08f93
daniellumertz <daniellumertz2@gmail.com>
2023-08-23 14:41:33 -0300

    midi toolkit typo fix

--- MIDI/MIDI Toolkit/DanielLumertz_MIDI Toolkit.lua
+++ MIDI/MIDI Toolkit/DanielLumertz_MIDI Toolkit.lua
@@ -1,4 +1,4 @@
--- @version 1.1
+-- @version 1.1.1
 -- @author Daniel Lumertz
 -- @provides
 --    [main=midi_editor] .
@@ -6,13 +6,13 @@
 --    [nomain] Fonts/*.ttf
 -- @license MIT
 -- @changelog
---    + Remove harmonic and melodic intervals at mapper. 
+--    + fix dofile typo. Thanks axel_ef! 
 
 
 --dofile("C:/Users/DSL/AppData/Roaming/REAPER/Scripts/Meus/Debug VS/DL Debug.lua")
 --demo = dofile(reaper.GetResourcePath() .. '/Scripts/ReaTeam Extensions/API/ReaImGui_Demo.lua')
 ScriptName = 'MIDI Toolkit'
-Version = '1.1'
+Version = '1.1.1'
 
 --- Load functions
 local info = debug.getinfo(1, 'S');
@@ -22,7 +22,7 @@ dofile(ScriptPath .. 'Functions/Arrange Functions.lua') -- Functions for using t
 dofile(ScriptPath .. 'Functions/Copy Paste Functions.lua') -- Functions for using the markov in reaper
 dofile(ScriptPath .. 'Functions/General Lua Functions.lua') -- General Functions needed
 dofile(ScriptPath .. 'Functions/General Music Functions.lua') -- General Functions needed
-dofile(ScriptPath .. 'Functions/Gui Functions.lua') -- General Functions needed
+dofile(ScriptPath .. 'Functions/GUI Functions.lua') -- General Functions needed
 dofile(ScriptPath .. 'Functions/ImGUI Custom Widgets.lua') -- General Functions needed
 dofile(ScriptPath .. 'Functions/Imgui Functions.lua') -- General Functions needed
 dofile(ScriptPath .. 'Functions/Main Loop.lua') -- General Functions needed
