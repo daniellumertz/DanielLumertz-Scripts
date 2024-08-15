503737d6a1f261733f3c2de1d9a8402436b7bf3b
daniellumertz <daniellumertz2@gmail.com>
2024-08-15 20:51:33 -0300

    new reapack header

--- Generative/Clouds/daniellumertz_Clouds.lua
+++ Generative/Clouds/daniellumertz_Clouds.lua
@@ -6,7 +6,7 @@
 --    [nomain] Image/Cloud.png
 --    [nomain] Info/*.txt
 --    [nomain] User Presets/*.json
---    [nomain] User Settings/*.json
+--    [nomain] User Settings/*
 --    [effect] FX/daniellumertz_Clouds.jsfx
 -- @changelog
 --    + Initial tests
