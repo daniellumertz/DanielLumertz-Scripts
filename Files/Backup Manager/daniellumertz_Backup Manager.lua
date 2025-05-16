d3f036370413787f68cc26f15cd0528c6715d446
daniellumertz <daniellumertz2@gmail.com>
2025-05-16 16:24:25 -0300

    macfix

--- Files/Backup Manager/daniellumertz_Backup Manager.lua
+++ Files/Backup Manager/daniellumertz_Backup Manager.lua
@@ -1,41 +1,11 @@
--- @version 0.1.2b
+-- @version 0.1.2
 -- @author Daniel Lumertz
 -- @provides
 --    [nomain] DL Functions/*.lua
 --    [nomain] Manager Functions/*.lua
 -- @changelog
---    + Initial release
+--    + fix for macos get path
 -- @license MIT
-
--- Initialize ImGUI?
-package.path = package.path..';'..reaper.ImGui_GetBuiltinPath() .. '/?.lua'
-ImGui = require 'imgui' '0.9.3' 
-
--- Variables
-SCRIPT_NAME = 'Backup Manager'
-SCRIPT_V = '0.1.2b' 
-DL_version_btn = false -- if you turn this true, it will enable an >experimental< feature to limit the number of versions of each file. Seems to be working, but it is too slow, so I stopped developing it in lua.  
-
--- Import functions from DL functions folder
-package.path = package.path..';'..debug.getinfo(1, "S").source:match [[^@?(.*[\/])[^\/]-$]] .. "?.lua;" -- GET DIRECTORY FOR REQUIRE
-require("DL Functions.General Debug")
-require("DL Functions.General Files")
-require("DL Functions.REAPER Enumerate")
-require("DL Functions.REAPER Projects")
-require('DL Functions.General Number')
-require('DL Functions.General String')
-require('DL Functions.ImGui')
-require('DL Functions.General Table')
-require('DL Functions.URL')
-require('Manager Functions.Theme')
-require('Manager Functions.Main GUI')
-require('Manager Functions.Backup')
-require('Manager Functions.Misc')
-
--- Debug
---local VSDEBUG = dofile("c:/Users/DSL/.vscode/extensions/antoinebalaine.reascript-docs-0.1.12/debugger/LoadDebug.lua")
---VSDEBUG = dofile("c:/Users/liane/.vscode/extensions/antoinebalaine.reascript-docs-0.1.12/debugger/LoadDebug.lua")
-reaper.defer(DL_Manager.GUI)
 
 --[[
 MIT License
@@ -61,3 +31,34 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 ]]
+
+-- Initialize ImGUI?
+package.path = package.path..';'..reaper.ImGui_GetBuiltinPath() .. '/?.lua'
+ImGui = require 'imgui' '0.9.3' 
+
+-- Variables
+SCRIPT_NAME = 'Backup Manager'
+SCRIPT_V = '0.1.2' 
+DL_version_btn = false -- if you turn this true, it will enable an >experimental< feature to limit the number of versions of each file. Seems to be working, but it is too slow, so I stopped developing it in lua.  
+
+-- Import functions from DL functions folder
+package.path = package.path..';'..debug.getinfo(1, "S").source:match [[^@?(.*[\/])[^\/]-$]] .. "?.lua;" -- GET DIRECTORY FOR REQUIRE
+require("DL Functions.General Debug")
+require("DL Functions.General Files")
+require("DL Functions.REAPER Enumerate")
+require("DL Functions.REAPER Projects")
+require('DL Functions.General Number')
+require('DL Functions.General String')
+require('DL Functions.ImGui')
+require('DL Functions.General Table')
+require('DL Functions.URL')
+require('Manager Functions.Theme')
+require('Manager Functions.Main GUI')
+require('Manager Functions.Backup')
+require('Manager Functions.Misc')
+
+-- Debug
+--local VSDEBUG = dofile("c:/Users/DSL/.vscode/extensions/antoinebalaine.reascript-docs-0.1.12/debugger/LoadDebug.lua")
+--VSDEBUG = dofile("c:/Users/liane/.vscode/extensions/antoinebalaine.reascript-docs-0.1.12/debugger/LoadDebug.lua")
+reaper.defer(DL_Manager.GUI)
+
